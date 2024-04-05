e700649bdd1677a237984fb5299b9a2b180c2498
techevinicius <108283361+techevinicius@users.noreply.github.com>
2024-04-05 09:40:17 -0300

    css

--- main.js
+++ main.js
@@ -18,4 +18,4 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores [0].textContent = tempoObjetivo1 - tempoAtual;
