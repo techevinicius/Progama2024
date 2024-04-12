62c2b8fb2b1d852b0a2d0a1d7e461c2fd31f5f6e
techevinicius <108283361+techevinicius@users.noreply.github.com>
2024-04-12 09:19:29 -0300

    css

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
 
-contadores [0].textContent = tempoObjetivo1 - tempoAtual;
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
